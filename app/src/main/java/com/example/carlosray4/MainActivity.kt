93e6f17fa2cc10be0456aba34d511f8b5c806b4b
airapet13 <oganyanairapet@gmail.com>
2021-10-19 17:21:27 +0300

    After MVVM/Volley

--- app/src/main/java/com/example/carlosray4/MainActivity.kt
+++ app/src/main/java/com/example/carlosray4/MainActivity.kt
@@ -6,28 +6,18 @@ import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import com.example.carlosray4.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import java.lang.Exception
-import org.json.JSONException
-
-import org.json.JSONObject
-
-
-
 
 
 class MainActivity : AppCompatActivity() {
-    var a: String = ""
+
 
     private lateinit var vm: MainViewModel
 
@@ -41,13 +31,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         Log.e("AAA","Activity created")
-        vm = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        vm = ViewModelProvider(this, MainViewModelFactory(this))
+            .get(MainViewModel::class.java)
 
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_web
@@ -60,35 +50,13 @@ class MainActivity : AppCompatActivity() {
         val reload: Button = findViewById(R.id.reload)
         val count: TextView = findViewById(R.id.count)
         val jokes: TextView = findViewById(R.id.jokes)
-        val URL = "https://api.icndb.com/jokes/random?"
-        reload.setOnClickListener {
-            jokes.text = ""
-            getJoke(count.text.toString().toInt(), URL)
-        }
-    }
 
-    fun getJoke(count: Int, url: String) {
-        var x = 0
-        val jokes: TextView = findViewById(R.id.jokes)
-        while(x < count){
-            x+=1
-            val queue = Volley.newRequestQueue(this)
-//        val url = "https://api.icndb.com/jokes/random?"
-            val stringRequest = StringRequest(
-            Request.Method.GET, url,
-            Response.Listener<String> { response ->
-//                a = ("${response}")
-                a = response.toString()
-            },
-            Response.ErrorListener { a = "That didn't work!"})
-            queue.add(stringRequest)
-            try {
-                a = a.replace("&quot;", "\"")
-                val jsonObject = JSONObject(a)
-                jokes.text = jokes.text.toString() + jsonObject.getJSONObject("value").getString("joke") + "\n"
-            } catch (e: JSONException) {
-                e.printStackTrace()
-            }
+        vm.resultLive.observe(this, Observer {
+            jokes.text = it
+        })
+
+        reload.setOnClickListener {
+            vm.getJoke(count.text.toString().toInt())
         }
     }
 }
--- app/src/main/java/com/example/carlosray4/MainViewModel.kt
+++ app/src/main/java/com/example/carlosray4/MainViewModel.kt
@@ -1,17 +1,54 @@
 package com.example.carlosray4
 
+import android.content.Context
 import android.util.Log
+import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
 
-class MainViewModel : ViewModel() {
+class MainViewModel(private val context: Context) : ViewModel() {
+
+    var resultLiveMutable = MutableLiveData<String>()
+    var resultLive: LiveData<String> = resultLiveMutable
+    var a: String = ""
+    val url = "https://api.icndb.com/jokes/random?"
+
+    fun getJoke(count: Int) {
+        var jokes = ""
+        val queue = Volley.newRequestQueue(context)
+        for (i in 1..count) {
+            val stringRequest = StringRequest(
+                Request.Method.GET, url,
+                Response.Listener<String?> { response ->
+                    a = response.toString()
+                },
+                Response.ErrorListener { a = "That didn't work!" })
+            queue.add(stringRequest)
+            try {
+                Log.e("AAA", "Шутка: $a")
+                val jsonObject = JSONObject(a)
+                jokes = jokes + jsonObject.getJSONObject("value").getString("joke") + "\n\n"
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        }
+        resultLiveMutable.value = jokes.replace("&quot;", "\"")
+    }
 
     init {
         Log.e("AAA","VM created")
+        getJoke(1)
     }
 
     override fun onCleared() {
-        Log.e("AAA","VM cleated")
+        Log.e("AAA","VM cleared")
         super.onCleared()
     }
-
 }
